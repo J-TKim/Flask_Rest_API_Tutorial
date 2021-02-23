501d04d70792c197cc84adfbde938e3893d7ca1f
J-TKim <kkkd991020@naver.com>
2021-02-23 17:48:35 +0900

    Create main.py, test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import requests
+
+
+BASE = 'http://127.0.0.1:5000/'
+
+response = requests.put(BASE + 'video/1', {'name': 'Tim', 'views': 100000, 'likes': 10})
+print(response.json())
+
+input()
+
+response = requests.get(BASE + 'video/1')
+print(response.json())
