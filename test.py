6aad4c8e3302c876bdaa8e1ac6295d9166d39733
J-TKim <kkkd991020@naver.com>
2021-02-24 10:47:41 +0900

    Upload

--- test.py
+++ test.py
@@ -3,10 +3,5 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.put(BASE + 'video/1', {'name': 'Tim', 'views': 100000, 'likes': 10})
-print(response.json())
-
-input()
-
-response = requests.get(BASE + 'video/1')
+response = requests.patch(BASE + 'video/2', {})
 print(response.json())
